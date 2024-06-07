4ac5088795992b0d77069fe7e92cebf0d65e1dc9
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-07 21:15:49 +0900

    Homeページに、ランダムに3つのイベント情報を表示させる。

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeEvents from "@/components/HomeEvents";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeEvents />
     </>
   );
 };
