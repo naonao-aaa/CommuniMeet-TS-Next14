0596df3e874a3afa42f2b5e2b2ba881ecdd8935a
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-08 14:24:05 +0900

    Event一覧ページで、Event一覧GET APIを叩いてデータを表示させる。

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,32 @@
-import events from "@/events.json";
 import EventCard from "@/components/EventCard";
+import { Event } from "@/types/event"; // Event 型定義のインポート
 
-const EventsPage: React.FC = () => {
+// eventデータをAPIから非同期に取得する関数
+async function fetchEvents(): Promise<Event[]> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/events`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return []; // エラーが発生した場合は空の配列を返す
+  }
+}
+
+const EventsPage: React.FC = async () => {
+  const events = await fetchEvents(); // eventデータを取得
+
+  // 取得したイベントを、作成日で降順にソート
+  events.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
