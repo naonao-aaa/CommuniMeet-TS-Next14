ed05f1dd3352c63a536803a0c12214778b93ed07
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-11 08:12:42 +0900

    ログイン時に、該当ユーザーを、データベースとセッションに保存 既にデータベースに登録されているユーザーの場合は、再度DBに登録しないようにしている。

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,5 +1,12 @@
+import connectDB from "@/config/database"; // MongoDBへの接続設定をインポート
+import User from "@/models/User"; // Userモデルをインポート
 import GoogleProvider from "next-auth/providers/google"; // Google認証プロバイダをインポート
-import { NextAuthOptions } from "next-auth"; //NextAuth.jsを設定する際に使用されるオプションオブジェクトの型をインポート
+import { NextAuthOptions, User as NextAuthUser, Profile } from "next-auth"; //NextAuth.jsを設定する際に使用されるオプションオブジェクトの型をインポート
+
+// Googleのプロファイルオブジェクトにpictureプロパティを追加するカスタム型
+interface GoogleProfile extends Profile {
+  picture?: string;
+}
 
 export const authOptions: NextAuthOptions = {
   // 認証プロバイダの配列
@@ -20,16 +27,36 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     // ユーザーが成功裏にサインインしたときに呼ばれる
     async signIn({ profile }) {
+      // カスタム型としてGoogleProfileを使用
+      const googleProfile = profile as GoogleProfile;
+
       // データベースに接続
+      await connectDB();
       // ユーザーが存在するか確認
+      const userExists = await User.findOne({ email: googleProfile?.email });
       // 存在しない場合はデータベースにユーザーを追加
+      if (!userExists) {
+        const username = googleProfile?.name?.slice(0, 20); // ユーザー名が長すぎる場合は、切り捨て
+
+        await User.create({
+          email: googleProfile?.email,
+          username,
+          image: googleProfile?.picture,
+        });
+      }
       // trueを返してサインインを許可
       return true;
     },
     // セッションオブジェクトを変更するための関数
     async session({ session }) {
       // データベースからユーザーを取得
+      const user = await User.findOne({ email: session.user?.email });
       // セッションにユーザーIDを割り当て
+      if (user) {
+        (session.user as NextAuthUser & { id: string }).id =
+          user._id.toString();
+      }
+      // session.user.id = user._id.toString();
       // 変更したセッションを返す
       return session;
     },
