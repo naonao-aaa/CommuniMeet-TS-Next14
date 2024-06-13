4b854e2c64dcd968743d22bb7483902c774fdaa6
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-14 07:30:51 +0900

    画像アップロード処理を、Event新規作成POST APIに実装。 「Cloudinary」というサービスに、画像アップロードしている。 npm install cloudinary

---
+++ config/cloudinary.ts
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from "cloudinary"; // CloudinaryのNode.js SDKをインポートする。`v2` はバージョン2を用いたいから。
+
+// Cloudinaryライブラリに対して設定を行う。
+// これにより、以後のAPI呼び出しでこれらの設定を使用することができる。
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // `cloud_name` はあなたのCloudinaryアカウントの一意のクラウド名。
+  api_key: process.env.CLOUDINARY_API_KEY, // `api_key` はCloudinary APIを利用するためのAPIキー。
+  api_secret: process.env.CLOUDINARY_API_SECRET, // `api_secret` はCloudinary APIを利用するための秘密キー。
+});
+
+// 設定済みのcloudinaryオブジェクトをエクスポート。
+// これにより、他のファイルからこのオブジェクトをインポートして直接使用することができる。
+export default cloudinary;
