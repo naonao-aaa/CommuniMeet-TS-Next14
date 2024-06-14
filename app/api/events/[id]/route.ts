f4dc2f34c9ab314258a6a1db15aac61aed09ff80
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-15 08:39:30 +0900

    Event削除用のDELETE APIを作成

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Event from "@/models/Event";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // URLパラメーターの型定義
 interface Params {
@@ -20,6 +21,49 @@ export const GET = async (request: Request, { params }: { params: Params }) => {
       status: 200,
     });
   } catch (error) {
+    console.log(error);
+    return new Response("Something Went Wrong", { status: 500 });
+  }
+};
+
+// DELETE /api/events/:id
+export const DELETE = async (
+  request: Request,
+  { params }: { params: Params }
+) => {
+  try {
+    const eventId = params.id; // リクエストから、イベントIDを取得。
+
+    const sessionUser = await getSessionUser(); // セッションユーザーを取得する関数を非同期で呼び出す。
+
+    // セッション確認
+    if (!sessionUser || !sessionUser.userId) {
+      // 「セッションユーザーが存在しない場合」または「ユーザーIDが存在しない場合」
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser; // セッションユーザーからユーザーIDを抽出。
+
+    await connectDB(); // データベースに接続。
+
+    const event = await Event.findById(eventId); // MongoDBから、指定されたIDでイベントを検索。
+
+    if (!event) return new Response("Event Not Found", { status: 404 }); // イベントが存在しない場合は404エラーを返す。
+
+    // 所有権の検証
+    if (event.owner.toString() !== userId) {
+      // イベントの所有者がセッションのユーザーIDと一致しない場合
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await event.deleteOne(); // イベントのドキュメントを削除。
+
+    // 成功レスポンス
+    return new Response("Event Deleted", {
+      status: 200,
+    });
+  } catch (error) {
+    // エラーが発生した場合
     console.log(error);
     return new Response("Something Went Wrong", { status: 500 });
   }
