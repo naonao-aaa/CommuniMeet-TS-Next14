5538c762634997eac9d410c7d84a4714586f3c05
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-16 10:58:46 +0900

    Bookmarkボタンの切り替え（Bookmarkボタンの、追加用ボタンと削除用ボタンの切り替え）

--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -17,6 +17,43 @@ const BookmarkButton: React.FC<BookmarkButtonProps> = ({ event }) => {
   const userId = session?.user?.id; // セッションからユーザーIDを取得
 
   const [isBookmarked, setIsBookmarked] = useState(false); // ブックマークされているかの状態を管理
+  const [loading, setLoading] = useState(true); // ローディング状態を管理するstate
+
+  useEffect(() => {
+    // ユーザーIDがなければ
+    if (!userId) {
+      setLoading(false); // ローディングを終了
+      return; // 処理を中断
+    }
+
+    // ブックマーク状態を確認する関数を定義。
+    const checkBookmarkStatus = async () => {
+      try {
+        // ブックマーク確認APIにリクエスト
+        const res = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            eventId: event._id, // イベントIDを、JSON形式で送信
+          }),
+        });
+
+        // レスポンスが200(成功)の場合
+        if (res.status === 200) {
+          const data = await res.json(); // レスポンスデータをJSONとしてパース
+          setIsBookmarked(data.isBookmarked); // ブックマーク状態を更新
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkBookmarkStatus(); // 実際に、ブックマーク状態確認関数を実行
+  }, [event._id, userId]);
 
   // Bookmarkボタンクリック時に走る関数
   const handleClick = async () => {
@@ -53,7 +90,18 @@ const BookmarkButton: React.FC<BookmarkButtonProps> = ({ event }) => {
     }
   };
 
-  return (
+  if (loading) return <p className="text-center">Loading...</p>;
+
+  return isBookmarked ? (
+    // ブックマーク済みの場合のボタン
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> ブックマーク解除
+    </button>
+  ) : (
+    // 未ブックマークの場合のボタン
     <button
       onClick={handleClick}
       className="bg-emerald-500 hover:bg-emerald-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
