a85a86b65532f73e58f6aa686a538f70c045d6ff
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-16 11:14:54 +0900

    ログインユーザーがブックマークしているイベント一覧を取得するGET APIを作成。

--- app/api/bookmarks/route.ts
+++ app/api/bookmarks/route.ts
@@ -3,7 +3,36 @@ import User from "@/models/User"; // Userモデルをインポート
 import Event from "@/models/Event"; // Eventモデルをインポート
 import { getSessionUser } from "@/utils/getSessionUser"; // セッションユーザー取得用の関数をインポート
 
-export const dynamic = "force-dynamic"; ////キャッシュを利用せずに、リクエスト毎に最新のデータを取得するための記述。
+export const dynamic = "force-dynamic"; // キャッシュを利用せずに、リクエスト毎に最新のデータを取得するための記述。
+
+// ログインユーザーがブックマークしているイベント一覧を取得するためのAPI
+// GET /api/bookmarks
+export const GET = async () => {
+  try {
+    await connectDB(); // データベースへの接続を確立。
+
+    const sessionUser = await getSessionUser(); // セッションからユーザー情報を取得。
+
+    // 「セッション情報がない場合」または「ユーザーIDがない場合」
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 }); // 401ステータスで応答し、ユーザーIDが必要であることを通知。
+    }
+
+    const { userId } = sessionUser; // ユーザーIDをセッションから抽出。
+
+    // データベースからユーザー情報を取得。
+    const user = await User.findOne({ _id: userId });
+
+    //ユーザーのブックマークリストにあるイベントIDをもとに、イベント情報を取得。
+    const bookmarks = await Event.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 }); // 取得したブックマークの情報をJSON形式でレスポンス。
+  } catch (error) {
+    // エラーが発生した場合の処理
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
 
 //イベントのブックマークを、追加または削除するためのAPIエンドポイント
 export const POST = async (request: Request) => {
