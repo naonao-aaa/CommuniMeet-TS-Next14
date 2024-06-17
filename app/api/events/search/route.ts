5bca606d5fa1cdc0a1217538f451dfcc05b827d1
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-17 20:49:42 +0900

    [修正]検索APIエンドポイント パラメータにkeyword がなくても、パラメータeventTypeに基づいてイベントを検索することができるように修正

--- app/api/events/search/route.ts
+++ app/api/events/search/route.ts
@@ -3,7 +3,7 @@ import Event from "@/models/Event";
 
 // queryの型を拡張
 interface QueryType {
-  $or: Array<{ [key: string]: RegExp }>;
+  $or?: Array<{ [key: string]: RegExp }>;
   type?: RegExp;
 }
 
@@ -18,28 +18,31 @@ export const GET = async (request: Request) => {
     const keyword = searchParams.get("keyword");
     const eventType = searchParams.get("eventType");
 
-    //keyword文字列を基に、正規表現パターンを作成（大文字小文字を区別しない）
-    const keywordPattern = keyword ? new RegExp(keyword, "i") : null;
+    // MongoDBに送信されるクエリの条件を格納する変数。空のオブジェクトで初期化している。
+    let query: QueryType = {};
+    // 検索キーワードに基づいて、動的に条件を格納するための配列。
+    const conditions = [];
 
-    // データベース内の複数フィールドにマッチするクエリを構築
-    // ユーザーが入力したキーワードに基づいて、様々なフィールド内で一致するデータを見つけるクエリを構築
-    // keywordPattern が null でない場合、イベントの name, description, location.venue, location.street, location.city, location.state, location.zipcode の各フィールドに対して指定されたパターン（正規表現）が含まれているかどうかを検証する。
-    // $or 配列にこれらの条件を追加することで、これらのどれか一つでも条件を満たすドキュメントが検索結果として返されるようになる。
-    let query: QueryType = {
-      $or: [
-        ...(keywordPattern
-          ? [
-              { name: keywordPattern },
-              { description: keywordPattern },
-              { "location.venue": keywordPattern },
-              { "location.street": keywordPattern },
-              { "location.city": keywordPattern },
-              { "location.state": keywordPattern },
-              { "location.zipcode": keywordPattern },
-            ]
-          : []),
-      ],
-    };
+    // keywordパラメータが存在する場合
+    if (keyword) {
+      const keywordPattern = new RegExp(keyword, "i"); //keyword文字列を基に、正規表現パターンを作成（大文字小文字を区別しない）
+      // keywordPatternに一致するかを調べる各フィールド（イベント名、説明、会場名など）に対して、検索条件を追加。
+      // このconditions配列は、後ほど$orクエリに組み込まれ、入力されたキーワードがこれらのどのフィールドかに一致するイベントを検索するために用いる。
+      conditions.push(
+        { name: keywordPattern },
+        { description: keywordPattern },
+        { "location.venue": keywordPattern },
+        { "location.street": keywordPattern },
+        { "location.city": keywordPattern },
+        { "location.state": keywordPattern },
+        { "location.zipcode": keywordPattern }
+      );
+    }
+
+    // conditions配列に何らかの条件が追加されていれば、queryオブジェクトに$orプロパティを設定し、その値としてconditions配列を割り当てる。
+    if (conditions.length > 0) {
+      query.$or = conditions;
+    }
 
     // eventTypeが'All'でなければ、そのタイプでさらにフィルタリング。
     if (eventType && eventType !== "All") {
