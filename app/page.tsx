11b831afed0fd15806b67d732c16bc7b853c6aec
naonao-aaa <naonao1814naonao@gmail.com>
2024-06-30 12:32:20 +0900

    [create]注目イベントを表示させるためのコンポーネント

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeEvents from "@/components/HomeEvents";
+import FeaturedEvents from "@/components/FeaturedEvents";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedEvents /> {/* 注目イベント */}
       <HomeEvents />
     </>
   );
---
+++ components/FeaturedEvents.tsx
@@ -0,0 +1,26 @@
+import { fetchFeaturedEvents } from "@/utils/requests"; //注目イベントを取得する関数をインポート
+import FeaturedEventCard from "./FeaturedEventCard";
+
+const FeaturedEvents: React.FC = async () => {
+  // 注目イベント(is_featuredがtrueのイベント)を取得する。
+  const events = await fetchFeaturedEvents();
+
+  return (
+    events.length > 0 && ( // 注目イベントが1件以上ある場合に表示させる。
+      <section className="bg-cyan-50 px-4 pt-6 pb-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-cyan-500 mb-6 text-center">
+            注目のイベント
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {events.map((event) => (
+              <FeaturedEventCard key={event._id} event={event} />
+            ))}
+          </div>
+        </div>
+      </section>
+    )
+  );
+};
+
+export default FeaturedEvents;
